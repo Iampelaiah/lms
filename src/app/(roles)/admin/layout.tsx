33868c998c77e8c443ae66b7edb1c87d9bdefb7f
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-09-11 21:19:15 +0000

    move this to the left of the screen (_for element <Button>_)

--- src/app/(roles)/admin/layout.tsx
+++ src/app/(roles)/admin/layout.tsx
@@ -32,7 +32,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
       <SidebarInset>
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <SidebarTrigger className="sm:hidden" />
-            <div className="hidden sm:flex items-center gap-2 ml-auto">
+            <div className="hidden sm:flex items-center gap-2">
                 <SidebarTrigger />
             </div>
         </header>
